f84132d7f1a9a514966f1e32000ed4071124847e
Michal Bojanowski <michal2992@gmail.com>
2022-10-04 16:39:29 +0200

    Add summary.NSUM()
    Add summary() method for objects returned by nsum.mcmc() and an
    associated print() method.

---
+++ R/summary.NSUM.R
@@ -0,0 +1,34 @@
+#' Summarise NSUM model fits
+#'
+#' [summary()] method for models fitted with [nsum.mcmc()].
+#'
+#' @param object object of class "`NSUM`" as returned by [nsum.mcmc()]
+#' @param x object of class "`NSUM.summary` as returned by `summary.NSUM()`
+#' @param ... other arguments to/from other methods, currently ignored
+#'
+#' @return The function `summary.NSUM` returns a list with as many elements as
+#'   `object` has, but elements corresponding to estimated parameters are
+#'   summarised with [summary.default()].
+#'
+#' @export
+
+summary.NSUM <- function(object, ...) {
+  nams <- c("NK", "d", "mu", "sigma", "rho", "tauK", "q")
+  has <- names(object) %in% paste0(nams, ".values")
+  rval <- object
+  rval[has] <- lapply(object[has], apply, 1, summary.default)
+  rval
+}
+
+
+#' @rdname summary.NSUM
+#' @export
+
+print.summary.NSUM <- function(x, ...) {
+  cat("\nNSUM model\n")
+  if(!is.null(x$call)) {
+    cat("\nCall:\n", paste(deparse(x$call), sep = "\n", collapse = "\n"),
+        "\n\n", sep = "")
+  }
+  invisible(NULL)
+}
